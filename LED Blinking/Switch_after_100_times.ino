cb0752e51f8482649227f4ac0f461a39923cd9f1
DurjoyKumar177 <durjoykumar177@gmail.com>
2025-06-07 21:47:35 +0600

    Ardino LED Blinking code

---
+++ LED Blinking/Switch_after_100_times.ino
@@ -0,0 +1,24 @@
+const int Led1 = 10;
+const int Led2 = 12;
+int count = 100;
+
+void setup() {
+  pinMode(Led1, OUTPUT);
+  pinMode(Led2, OUTPUT);
+}
+
+void loop() {
+  for (int i = 0; i < count; i++) {
+    digitalWrite(Led2, HIGH);
+    delay(50);
+    digitalWrite(Led2, LOW);
+    delay(50);
+  }
+
+  for (int i = 0; i < count; i++) {
+    digitalWrite(Led1, HIGH);
+    delay(50);
+    digitalWrite(Led1, LOW);
+    delay(50);
+  }
+}
